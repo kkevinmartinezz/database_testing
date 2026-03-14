550805d977b6cc56df65cff417efe24035bcc5d0
kkevinmartinezz <kevin425.m@hotmail.com>
2026-03-14 00:38:30 -0400

    Learning how to begin a mongoDB connection. Realizing I need to install a couple of things before actually beginning to use mongoDB. Will continue mongoDB installations later. After that I should be able to run the command [ mongosh ] and then run the database.py file and be able to do stuff with my database

---
+++ src/mongoDB/database.py
@@ -0,0 +1,24 @@
+import pymongo #need in order to use mongodb
+
+myClient = pymongo.MongoClient('mongodb://localhost:27017/') #create a mongo client object
+
+print(myClient)
+
+mydb = myClient['mydatabase'] #creating a database from the client established earlier
+# the database above will still not be ceeated UNTIL content is put inside it
+
+#we will not add content to the database created above
+#a collection is the same as a Table in SQL databases
+collection = mydb['collection']
+# same as database, collection is created after filling it with content (via documents)
+
+#Now we will add content to collection
+#documents are same as records in SQL
+mydict = {"name": "John", "age": 25}
+x = collection.insert_one(mydict)
+print(x.inserted_id) #inserted_id will return the unique _id of a document inserted by insert_one()
+
+
+
+# print(myClient.list_database_names())
+# print(mydb.list_collection_names())
